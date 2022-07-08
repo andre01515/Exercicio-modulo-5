69379e2419684783f0e0fca90cd06c7e034c225c
andre01515 <carrao.andre@hotmail.com>
2022-07-08 16:03:38 -0300

    Exercício módulo 5

--- HelloWordl.java
+++ HelloWordl.java
@@ -1,7 +1,5 @@
 public class HelloWordl{
     public static void main(String[] args) {
-        String ambiente = args[0];
-        
-        System.out.print(ambiente);
+        System.out.print("Hello world!");
     }
 }
